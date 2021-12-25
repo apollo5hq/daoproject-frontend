346a95b71acf13812c37628750b5548d87af7691
Conrad Stephens <conradastephens@gmail.com>
2021-12-25 12:57:15 -0500

    added tests for web3 reducer

--- __tests__/ConnectButton.test.tsx
+++ __tests__/ConnectButton.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "../utils/test-utils";
 import ConnectButton from "../src/components/ConnectButton";
 
 describe("ConnectButton", () => {
