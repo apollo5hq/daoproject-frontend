a3a60e099ed8ca83aad2a41b718768431bd03527
Conrad Stephens <conradastephens@gmail.com>
2022-01-10 13:07:13 -0500

    added link to canvas page on the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,36 @@
 import { ConnectButton } from "@/components";
 import { Typography, Box, Button, Container, styled } from "@mui/material";
 import { useAppSelector } from "src/redux/app/hooks";
+import Link from "next/link";
 
 const GetStartedButton = styled(Button)({
   textTransform: "none",
 });
 
+const PageContainer = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  height: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 export default function Index() {
   const address = useAppSelector((state) => state.web3.data.address);
   return (
-    <Container
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      maxWidth="lg"
-    >
+    <PageContainer maxWidth="lg">
       <Box sx={{ my: 4 }}>
         <Typography variant="h2">Welcome to DAO Project</Typography>
       </Box>
       <Box sx={{ my: 4 }}>
         {address ? (
-          <GetStartedButton>Get Started</GetStartedButton>
+          <Link href="/canvas" passHref>
+            <GetStartedButton>Get Started</GetStartedButton>
+          </Link>
         ) : (
           <ConnectButton />
         )}
       </Box>
-    </Container>
+    </PageContainer>
   );
 }
