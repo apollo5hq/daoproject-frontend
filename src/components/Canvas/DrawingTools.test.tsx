915b989e0fcd8193c010c7d7f27595acbf7d132b
Conrad Stephens <conradastephens@gmail.com>
2022-01-23 10:04:35 -0500

    added another test for drawing tool

--- src/components/Canvas/DrawingTools.test.tsx
+++ src/components/Canvas/DrawingTools.test.tsx
@@ -1,10 +1,14 @@
-import { render, screen } from "utils/test-utils";
+import { render, screen, fireEvent } from "utils/test-utils";
 import { DrawingTool } from "@/components";
 
 describe("DrawingTool", () => {
   render(<DrawingTool name="Eraser" variant="outlined" onClick={() => {}} />);
-
+  let button: HTMLElement;
   it("renders the drawing tool", async () => {
-    await screen.findByTestId("drawingTool");
+    button = await screen.findByTestId("drawingTool");
+  });
+
+  it("clicking drawing tool results in click action", async () => {
+    fireEvent.click(button);
   });
 });
