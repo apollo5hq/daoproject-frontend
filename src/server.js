6b281966cc72885cd6034d0275db5d045f199cc3
Conrad Stephens <conradastephens@gmail.com>
2022-01-28 08:48:22 -0500

    removed gun config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const next = require("next");
-const Gun = require("Gun");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -10,16 +9,12 @@ const handle = client.getRequestHandler();
 client.prepare().then(() => {
   const app = express();
 
-  app.use(Gun.serve);
-
   app.all("*", (req, res) => {
     return handle(req, res);
   });
 
-  const server = app.listen(port, (err) => {
+  app.listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
-
-  Gun({ web: server });
 });
